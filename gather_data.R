f8d4c5c333ddf007ffa9b206b8c47806578915b2
ZackDrum <zdrum@unc.edu>
2025-03-14 14:44:02 -0400

    Update gather_data.R

--- gather_data.R
+++ gather_data.R
@@ -20,11 +20,10 @@ if(using_googlesheets){
   
   
   position_data <- read_sheet(positions_sheet_loc, sheet = "positions",skip = 1)
-  online        <- read_sheet(positions_sheet_loc, sheet = "online",skip = 1)
+  online        <- read_sheet(positions_sheet_loc, sheet = "contact_info",skip = 1)
   text_blocks   <- read_sheet(positions_sheet_loc, sheet = "text_blocks",skip = 1)
   contact_info  <- read_sheet(positions_sheet_loc, sheet = "contact_info", skip = 1)
-  reference  <- read_sheet(positions_sheet_loc, sheet = "reference", skip = 1)
-  skills  <- read_sheet(positions_sheet_loc, sheet = "skills", skip = 1)
+  skills  <- read_sheet(positions_sheet_loc, sheet = "laboratory_skills", skip = 1)
   
 } else {
   
